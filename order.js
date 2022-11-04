06e7fb75183e8e793c2bb149bcec0dfec425cad0
shyanukant rathi <rathishyanukant@gmail.com>
2022-11-04 17:08:46 +0530

    dashboard page

---
+++ order.js
@@ -0,0 +1,38 @@
+const orders = [
+    {
+        productName: 'Laptop',
+        productNumber : '8739',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }, {
+        productName: 'Mobile',
+        productNumber: '7739',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Tablet',
+        productNumber: '8539',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone',
+        productNumber: '7658',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'PS5',
+        productNumber: '9739',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Laptop',
+        productNumber: '3739',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+
+]
